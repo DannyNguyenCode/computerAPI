76fe396e28bd7299090ad2e52854f29209993563
Gia Bao Nguyen <giabnguyen1@gmail.com>
2020-10-08 23:15:53 -0400

    added functions to begin communicating with mongodb database

--- data-service.js
+++ data-service.js
@@ -1,36 +1,95 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const amdProcessorSchema = require("./models/amdprocessors.js");
-const intelProcessorSchema = require("./models/intelprocessors.js");
+const amdgpuSchema = require('./models/amdgpu.js');
+const amdMoboSchema = require('./models/amdmobo');
+const amdProcessorSchema = require('./models/amdprocessors');
+const caseFanSchema = require('./models/casefan');
 
 module.exports = function (mongoDBConnectionString) {
-  let intelProcessors;
-  let amdProcessors;
+  let Amdgpus;
+  let Amdmobos;
+  let Amdprocessors;
+  let CaseFan;
   return {
-    connect: function () {
-      return new Promise(function (resolve, reject) {
+    connect: () => {
+      return new Promise((resolve, reject) => {
         let db = mongoose.createConnection(mongoDBConnectionString, {
           useNewUrlParser: true,
           useUnifiedTopology: true,
         });
-
-        db.on("error ", (err) => {
+        db.on('error ', (err) => {
           reject(err);
         });
 
-        db.once("open", () => {
-          amdProcessors = db.model("Amdprocessor", amdProcessorSchema);
-          intelProcessors = db.model("Intelprocessors", intelProcessorSchema);
+        db.once('open', () => {
+          Amdgpus = db.model('Amdgpu', amdgpuSchema);
+          Amdmobos = db.model('Amdmobo', amdMoboSchema);
+          Amdprocessors = db.model('Amdprocessor', amdProcessorSchema);
+          CaseFan = db.model('Casefan', caseFanSchema);
+          console.log('Connection successfully in connect function');
           resolve();
         });
       });
     },
-    getAMDProcessors: function () {
-      return new Promise(function (resolve, reject) {
-        amdProcessors
-          .find()
+    getAllAMDGPUs: () => {
+      return new Promise((resolve, reject) => {
+        Amdgpus.find()
+          //.sort({}) //optional "sort" - https://docs.mongodb.com/manual/reference/operator/aggregation/sort/
+          .exec()
+          .then((gpu) => {
+            resolve(gpu);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    getAMDGPUsById: (gpuId) => {
+      return new Promise((resolve, reject) => {
+        Amdgpus.find({ _id: gpuId })
+          .limit(1)
+          .exec()
+          .then((gpu) => {
+            resolve(gpu);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    getAllAMDmobos: () => {
+      return new Promise((resolve, reject) => {
+        Amdmobos.find()
+          //.sort({}) //optional "sort" - https://docs.mongodb.com/manual/reference/operator/aggregation/sort/
+          .exec()
+          .then((mobo) => {
+            resolve(mobo);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    getAMDmobosById: (amdmoboid) => {
+      return new Promise((resolve, reject) => {
+        Amdmobos.find({ _id: amdmoboid })
+          .limit(1)
+          .exec()
+          .then((mobo) => {
+            resolve(mobo);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+
+    getAllAMDProcessors: () => {
+      return new Promise((resolve, reject) => {
+        Amdprocessors.find()
+          //.sort({}) //optional "sort" - https://docs.mongodb.com/manual/reference/operator/aggregation/sort/
           .exec()
           .then((processors) => {
             resolve(processors);
@@ -40,13 +99,39 @@ module.exports = function (mongoDBConnectionString) {
           });
       });
     },
-    getIntelProcessors: function () {
-      return new Promise(function (resolve, reject) {
-        intelProcessors
-          .find()
+
+    getAMDProcessorsById: (amdprocessorid) => {
+      return new Promise((resolve, reject) => {
+        Amdprocessors.find({ _id: amdprocessorid })
+          .limit(1)
           .exec()
-          .then((processors) => {
-            resolve(processors);
+          .then((proc) => {
+            resolve(proc);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    getAllCaseFans: () => {
+      return new Promise((resolve, reject) => {
+        CaseFan.find()
+          .exec()
+          .then((casefan) => {
+            resolve(casefan);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    getCaseFanById: (casefanid) => {
+      return new Promise((resolve, reject) => {
+        CaseFan.find({ _id: casefanid })
+          .limit(1)
+          .exec()
+          .then((casefan) => {
+            resolve(casefan);
           })
           .catch((err) => {
             reject(err);
